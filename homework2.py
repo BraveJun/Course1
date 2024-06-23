e3abf63c293836deb804564bba02f4ae31fca0b6
BraveJun <mansshaih@mail.ru>
2024-06-23 18:50:43 +0300

    Переменные
    Практическое задание по теме "Переменные"

---
+++ homework2.py
@@ -0,0 +1,5 @@
+amount_of_homework = 12
+spent_hours = 1.5
+course_name = 'Python'
+time_on_task = spent_hours / amount_of_homework
+print('Курс: ', course_name, ', всего задач: ', amount_of_homework, ', затрачено часов: ', spent_hours, ', среднее время выполнения ', time_on_task, ' часа (', time_on_task * 60, ' минут)', sep='')
