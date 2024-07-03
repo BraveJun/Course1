65babb073310a871f840b1247fd588e162792299
BraveJun <mansshaih@mail.ru>
2024-07-03 23:39:22 +0300

    Функции в Python. Функция с параметром
    Домашняя работа по уроку "Функции в Python. Функция с параметром"

---
+++ module_2_5.py
@@ -0,0 +1,26 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for j in range(n):
+        stroki = []
+        matrix.append(stroki)
+        for k in range(m):
+            stroki.append(value)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+print('')
+
+for i in range(len(result1)):  # решил вывести матрицы визуально
+    print(result1[i])
+print('')
+for i in range(len(result2)):
+    print(result2[i])
+print('')
+for i in range(len(result3)):
+    print(result3[i])
